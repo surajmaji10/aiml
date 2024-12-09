a9241831f6c33b3b8d789ee6727c3ecf73371b69
akashmaji946 <akashmaji946@gmail.com>
2024-12-09 11:33:12 +0530

    more

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Akash Maji',
+  tagline: 'Simplicity is the best Sophistication',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -76,7 +76,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Akash Maji',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,36 +3,40 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  // {
-  //   title: 'M.Tech, CSA, IISc Bangalore',
-  //   Svg: require('@site/static/img/iisc.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus was designed from the ground up to be easily installed and
-  //       used to get your website up and running quickly.
-  //     </>
-  //   ),
-  // },
   {
-    title: 'B.Tech, CSE, TIT Bhopal',
-    img: require('@site/static/img/img_1.png').default,
+    title: 'M.Tech, CSA, IISc Bangalore',
+      subtitle: 'Aug 2024 - June 2026 | Full Time',
+      img: require('@site/static/img/iisc-logo.png').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+       Pursuing M.Tech at Computer Science and Automation Department of Indian Institute of Science, Bangalore.
       </>
     ),
   },
-  // {
-  //   title: 'Ex-TCSer, System Engineer, TCS Indore',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
+
+    {
+        title: 'Ex-TCSer, System Engineer, TCS Indore',
+        subtitle: 'Aug 2021 - May 2024 | Full Time',
+        img: require('@site/static/img/tcs-logo.png').default,
+        description: (
+            <>
+                Worked as a System Engineer in TCS(Digital) as a Full Stack Java Web Developer.
+            </>
+        ),
+    },
+
+
+  {
+    title: 'B.Tech, CSE, RGPV Bhopal',
+      subtitle: 'Aug 2017 - June 2021 | Full Time',
+    img: require('@site/static/img/rgpv-logo.png').default,
+    description: (
+      <>
+          Pursued B.Tech at Computer Science and Engineering Department of Technocrats Institute, Bhopal.
+      </>
+    ),
+  },
+
 
     // {
     //     title: 'Powered by React 2',
@@ -46,7 +50,7 @@ const FeatureList = [
     // }
 ];
 
-function Feature({img, title, description}) {
+function Feature({img, title, description, subtitle}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -54,6 +58,7 @@ function Feature({img, title, description}) {
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
+          <Heading as="h5">{subtitle}</Heading>
         <p>{description}</p>
       </div>
     </div>
